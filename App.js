9fe66c4d5c04dbe3aa48bfda6b86df138d02a1f0
Khealgit08 <castilmichael08@gmail.com>
2025-11-05 11:51:44 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi! This is Kheal one of the Smart Scare Crow app developer.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
